6eb52bcb125c08aa73b0c96ca51bba0a5dccf966
Artem Bulychev <twist1996@icloud.com>
2023-01-23 18:54:20 +0500

    Добавлен адаптив

---
+++ scripts/script.js
@@ -0,0 +1,58 @@
+// Объявление переменных для profile
+let profileOpenButton = document.querySelector('.profile__rectangle');
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
+
+
+// Объявление переменных для popup
+let popupElement = document.querySelector('.popup'); 
+let formElement = document.querySelector('.popup__form-container');
+let popupClose = document.querySelector('.popup__close');
+let popupSave = document.querySelector('.popupSave');  
+let popupInputName = document.querySelector('.popup__name_input');
+let popupInputJob = document.querySelector('.popup__job_input'); 
+
+// Открытие 
+
+let popupOpen = function() {
+  popupElement.classList.add('popup_opened'); 
+  popupInputName.value = profileName.textContent; 
+  popupInputJob.value = profileJob.textContent; 
+}
+
+// Обработчик события при открытии 
+profileOpenButton.addEventListener('click', popupOpen); 
+
+// Закрытие 
+
+let popupClosed = function() {
+  popupElement.classList.remove('popup_opened')
+}
+
+//Обработчик события при закрытии 
+
+popupClose.addEventListener('click', popupClosed); 
+
+// Обработка формы отправки: 
+
+let handleFormSubmit = function (event) {
+  event.preventDefault();
+  profileName.textContent = popupInputName.value ; 
+  profileJob.textContent = popupInputJob.value; 
+  popupClosed ();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+
+// Реализация like-active 
+
+let likeActive = document.querySelector('.elements__like');
+let likeActiveColor = document.querySelector('.elements__like-active') 
+
+
+likeActive.addEventListener('click', function (event) {
+  const ev = event.target.closest('likeActiveColor');
+  if (ev) {
+    ev.classList.toggle('.elements__like-active');
+  }
+}); 
