f6cb18ff767b71d9d5e2dccc2c69559227ce4052
Artem Bulychev <twist1996@icloud.com>
2023-02-25 12:49:57 +0500

    6.5 commit

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,97 +1,88 @@
 
-// Функция добавления класса с ошибкой валидации
 
-const showInputError = (formElement, inputElement, errorMessage, config) => {
-  const errorClass = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add(config.inputErrorClass); 
-  errorClass.textContent = errorMessage; 
-  errorClass.classList.add(config.errorClass);
-}; 
+// Функция добавления класса с error
+const showError = (formElement, inputElement, errorMessage, config) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(config.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(config.errorClass);
+};
 
-// Функция скрытия класса с ошибкой при валидной форме
+// Функция удаления класса с error
+const hideError = (formElement, inputElement, config) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(config.inputErrorClass);
+  errorElement.textContent = "";
+  errorElement.classList.remove(config.errorClass);
+};
 
-const hideInputError = (formElement, inputElement, config) => {
-  const errorClass = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove(config.inputErrorClass); 
-  errorClass.textContent = '';
-  errorClass.classList.remove(config.errorClass);
-}; 
-
-// Функция проверки валидации 
-
-const checkInputValidity = (formElement, inputElement, config) => {
+// Функция проверки валидности
+const checkValidity = (formElement, inputElement, config) => {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage, config);
-
+    showError(formElement, inputElement, inputElement.validationMessage, config);
   } else {
-    hideInputError(formElement, inputElement, config); 
+    hideError(formElement, inputElement, config);
   }
 };
 
-// Функция на каждый ввод символа 
-const setListeners = (formElement, config) => {
+// Функция проверки на каждый вводимый символ в поле
+const setEventListeners = (formElement, config) => {
   const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
-  const buttonElement = formElement.querySelector(config.submitButtonSelector); 
-  toggleButton(inputList, buttonElement); 
-  inputList.forEach((inputSelector) => {
-    inputSelector.addEventListener('input', () => {
-      checkInputValidity(formElement, inputSelector, config);
-      toggleButton(inputList, buttonElement, config); 
+  const buttonSubmit = formElement.querySelector(config.submitButtonSelector);
+  toggleButton(inputList, buttonSubmit, config);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      checkValidity(formElement, inputElement, config);
+      toggleButton(inputList, buttonSubmit, config);
     });
   });
-};
-
-// Проверка на 1 из 2 невалидный инпут 
-
-const toggleButton = (inputList, buttonElem, config) => {
-  const isAnyInvalid = inputList.some((input) => !input.validity.valid);
-  const isBothFilled = inputList.every((input) => input.value !== '');
-  const isLengthValid = inputList.every((input) => input.value.length >= 2);
-
-  if (isAnyInvalid || !isBothFilled || !isLengthValid) {
-      buttonElem.classList.add(config.inactiveButtonClass);
-      buttonElem.disabled = true;
-      isSubmitButtonDisabled = true;
-  } else {
-      buttonElem.classList.remove(config.inactiveButtonClass);
-      buttonElem.disabled = false;
-      isSubmitButtonDisabled = false;
-  }
 };
 
 function setsubmitbutton(button, isValid) {
   button.disabled = !isValid;
 }
-// Массив полей 
 
-const hasInvalidInput = (inputList) => {
+// Функция активной/неактивной кнопки 
+const toggleButton = (inputList, buttonSubmit, config) => {
+  if (checkInvalidInput(inputList, config)) {
+
+    buttonSubmit.disabled = true;
+    buttonSubmit.classList.add(config.inactiveButtonClass);
+  } else {
+    buttonSubmit.removeAttribute("disabled", "disabled");
+    buttonSubmit.classList.remove(config.inactiveButtonClass);
+  }
+};
+
+
+// Функция массива полей
+const checkInvalidInput = (inputList) => {
   return inputList.some((inputElement) => {
-    return !inputElement.validity.valid; 
-  })
-}; 
-
+    return !inputElement.validity.valid;
+  });
+};
 
 const enableValidation = (config) => {
-  const formList = Array.from(document.querySelectorAll(lists.formSelector));
+  const formList = Array.from(document.querySelectorAll(".popup"));
   formList.forEach((formElem) => {
-      setListeners(formElem, lists);
+    formElem.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+    });
+    const fieldsetList = Array.from(
+      formElem.querySelectorAll(config.formSelector)
+    );
+    fieldsetList.forEach((field) => {
+      setEventListeners(field, config);
+    });
   });
 };
-const checkInvalidInput = (inputList) => {
-  return inputList.some((inputElem) => {
-      return !inputElem.validity.valid;
-  });
-};
-
-
 
 enableValidation ({
   formSelector: '.popup__form-container',
   inputSelector: '.popup__item',
   submitButtonSelector: '.popup__save',
   inactiveButtonClass: 'popup__save_inactive',
-  inputErrorClass: 'popup__item-error_active',
-  errorClass: 'popup__item-error',
+  inputErrorClass: 'popup__item_type_active',
+  errorClass: 'popup__item-error_active',
 });
-
-enableValidation(config);
